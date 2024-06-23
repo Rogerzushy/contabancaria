aa8d6aa1fed59655c5c9223a7f129d9dcfe2ffe7
zushy <rogersoeiro1357@gmail.com>
2024-06-23 19:08:01 -0300

    contabancaria

--- conta-banco/src/contaterminal.java
+++ conta-banco/src/contaterminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class contaterminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+    public static void main (String[] args){
+
+         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Qual o seu nome ");
+        String nome = scanner.next();
+
+        System.out.println("qual o numero do cartao ");
+        int numero = scanner.nextInt();
+
+        System.out.println("qual sua agencia  ");
+        String agencia = scanner.next();
+
+        System.out.println("Qual o seu saldo  ");
+        Double saldo = scanner.nextDouble();
+
+        System.out.println("Ola " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " +  agencia + " , conta 13" + numero + " e seu saldo " + saldo + " já está disponível para saque" );
+    }    
+    
 }
